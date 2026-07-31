c2bf06f800e0b2c669d4cd05ffe8754963c17a69
arunnissal <arunnissal45@gmail.com>
2026-07-31 14:43:46 +0530

    Bind Gunicorn to the  environment variable

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,4 @@ set -o errexit
 
 python manage.py migrate
 python manage.py seed_data
-gunicorn function_requirement_system.wsgi:application
+gunicorn --bind 0.0.0.0:${PORT:-8000} function_requirement_system.wsgi:application
